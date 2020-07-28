7372914b7a4964e27be4b49b21e3e0f6aa7b4950
EnWei <10646007@ntub.edu.tw>
2020-07-28 16:55:14 +0800

    重構 - 重構MultipartFileUpload

--- src/main/java/tw/edu/ntub/imd/camping/CampingApplication.java
+++ src/main/java/tw/edu/ntub/imd/camping/CampingApplication.java
@@ -8,8 +8,14 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import tw.edu.ntub.imd.camping.config.properties.FileProperties;
+import tw.edu.ntub.imd.camping.dto.file.directory.Directory;
+import tw.edu.ntub.imd.camping.dto.file.directory.DirectoryImpl;
+import tw.edu.ntub.imd.camping.dto.file.uploader.MultipartFileUploader;
 import tw.edu.ntub.imd.camping.service.BaseViewService;
 import tw.edu.ntub.imd.camping.util.http.ResponseUtils;
+
+import java.nio.file.Paths;
 
 @SpringBootApplication
 public class CampingApplication {
@@ -28,5 +34,15 @@ public class CampingApplication {
         AspectJExpressionPointcut expression = new AspectJExpressionPointcut();
         expression.setExpression("execution(* " + BaseViewService.class.getPackageName() + ".*.*(..))");
         return new DefaultPointcutAdvisor(expression, advice);
+    }
+
+    @Bean("fileDirectory")
+    public Directory fileDirectory(FileProperties fileProperties) {
+        return new DirectoryImpl(Paths.get(fileProperties.getPath()));
+    }
+
+    @Bean
+    public MultipartFileUploader multipartFileUploader(Directory fileDirectory, FileProperties fileProperties) {
+        return new MultipartFileUploader(fileDirectory, fileProperties.getUrl());
     }
 }
--- src/main/java/tw/edu/ntub/imd/camping/dto/file/uploader/MultipartFileUploader.java
+++ src/main/java/tw/edu/ntub/imd/camping/dto/file/uploader/MultipartFileUploader.java
@@ -11,47 +11,60 @@ import tw.edu.ntub.imd.camping.util.file.FileUtils;
 import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
-public class MultipartFileUploader implements Uploader {
-    private final String uploadBaseUrl;
-    private final MultipartFile multipartFile;
-    private final String[] subUrl;
+public class MultipartFileUploader {
+    private final Directory baseDirectory;
+    private final String baseURL;
 
-    public MultipartFileUploader(@Nonnull String uploadBaseUrl, @Nonnull MultipartFile multipartFile, String... subUrl) {
-        this.uploadBaseUrl = uploadBaseUrl;
-        this.multipartFile = multipartFile;
-        this.subUrl = subUrl;
+    public MultipartFileUploader(@Nonnull Directory baseDirectory, @Nonnull String baseURL) {
+        this.baseDirectory = baseDirectory;
+        this.baseURL = baseURL;
     }
 
     @Nonnull
-    @Override
-    public UploadResult upload(@Nonnull Directory uploadTo) throws FileException {
+    public UploadResult upload(@Nonnull MultipartFile multipartFile, String... subDirectoryName) throws FileException {
         if (multipartFile.isEmpty()) {
             throw new EmptyFileException(multipartFile.getName());
-        } else if (uploadTo.getAbsolutePath() == null) {
+        } else if (baseDirectory.getAbsolutePath() == null) {
             throw new NotHavePathException();
         }
-        if (uploadTo.isNotExist()) {
-            uploadTo.create();
+        Directory uploadTarget = baseDirectory;
+        for (String name : subDirectoryName) {
+            uploadTarget = uploadTarget.addDirectory(name);
+        }
+        if (uploadTarget.isNotExist()) {
+            uploadTarget.create();
         }
         try {
-            String fullRandomFileName = getFullRandomFileName();
-            Path absolutePath = uploadTo.getAbsolutePath();
+            String fullRandomFileName = getFullRandomFileName(multipartFile);
+            Path absolutePath = uploadTarget.getAbsolutePath();
             Path storePath = absolutePath.resolve(fullRandomFileName);
             multipartFile.transferTo(storePath);
             return UploadResult.builder()
                     .filePath(storePath)
-                    .url(uploadBaseUrl + "/" + String.join("/", subUrl) + "/" + fullRandomFileName)
+                    .url(baseURL + "/" + String.join("/", subDirectoryName) + "/" + fullRandomFileName)
                     .build();
         } catch (IOException e) {
             throw new FileUnknownException(e);
         }
     }
 
-    private String getFullRandomFileName() {
+    private String getFullRandomFileName(MultipartFile multipartFile) {
         String originalFilename = Objects.requireNonNull(multipartFile.getOriginalFilename());
         int firstDotIndex = originalFilename.indexOf(".");
         return FileUtils.getRandomFileName() + originalFilename.substring(firstDotIndex);
+    }
+
+    @Nonnull
+    public List<UploadResult> upload(@Nonnull MultipartFile[] fileArray, String... subDirectoryName) throws FileException {
+        return upload(List.of(fileArray), subDirectoryName);
+    }
+
+    @Nonnull
+    public List<UploadResult> upload(@Nonnull List<MultipartFile> fileArray, String... subDirectoryName) throws FileException {
+        return fileArray.parallelStream().map(file -> upload(file, subDirectoryName)).collect(Collectors.toList());
     }
 }
