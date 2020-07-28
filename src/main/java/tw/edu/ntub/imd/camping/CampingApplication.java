751c3bf82f8e29eca42a447664c0dd5c85cf4d44
EnWei <10646007@ntub.edu.tw>
2020-07-28 16:50:58 +0800

    修正 - 修正未加入ObjectMapper、Advisor的問題

--- src/main/java/tw/edu/ntub/imd/camping/CampingApplication.java
+++ src/main/java/tw/edu/ntub/imd/camping/CampingApplication.java
@@ -1,7 +1,15 @@
 package tw.edu.ntub.imd.camping;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.aopalliance.aop.Advice;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import tw.edu.ntub.imd.camping.service.BaseViewService;
+import tw.edu.ntub.imd.camping.util.http.ResponseUtils;
 
 @SpringBootApplication
 public class CampingApplication {
@@ -10,4 +18,15 @@ public class CampingApplication {
         SpringApplication.run(CampingApplication.class, args);
     }
 
+    @Bean
+    public ObjectMapper objectMapper() {
+        return ResponseUtils.createMapper();
+    }
+
+    @Bean
+    public DefaultPointcutAdvisor defaultPointcutAdvisor(@Qualifier("transactionInterceptor") Advice advice) {
+        AspectJExpressionPointcut expression = new AspectJExpressionPointcut();
+        expression.setExpression("execution(* " + BaseViewService.class.getPackageName() + ".*.*(..))");
+        return new DefaultPointcutAdvisor(expression, advice);
+    }
 }
