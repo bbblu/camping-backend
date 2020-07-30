339817146172dd98d66d85e0ef16e55e3bd0cb87
EnWei <10646007@ntub.edu.tw>
2020-07-30 16:46:29 +0800

    修正 - 修正無法序列化或反序列化自訂列舉的問題

--- src/main/java/tw/edu/ntub/imd/camping/CampingApplication.java
+++ src/main/java/tw/edu/ntub/imd/camping/CampingApplication.java
@@ -1,6 +1,10 @@
 package tw.edu.ntub.imd.camping;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.aopalliance.aop.Advice;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
@@ -11,12 +15,16 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import tw.edu.ntub.imd.camping.config.properties.FileProperties;
+import tw.edu.ntub.imd.camping.databaseconfig.enumerate.Experience;
+import tw.edu.ntub.imd.camping.databaseconfig.enumerate.Gender;
+import tw.edu.ntub.imd.camping.databaseconfig.enumerate.UserRoleEnum;
 import tw.edu.ntub.imd.camping.dto.file.directory.Directory;
 import tw.edu.ntub.imd.camping.dto.file.directory.DirectoryImpl;
 import tw.edu.ntub.imd.camping.dto.file.uploader.MultipartFileUploader;
 import tw.edu.ntub.imd.camping.service.BaseViewService;
 import tw.edu.ntub.imd.camping.util.http.ResponseUtils;
 
+import java.io.IOException;
 import java.nio.file.Paths;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
@@ -28,7 +36,48 @@ public class CampingApplication {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return ResponseUtils.createMapper();
+        ObjectMapper mapper = ResponseUtils.createMapper();
+        SimpleModule projectModule = new SimpleModule("projectModule",
+                new Version(1, 0, 0, "", "tw.edu.ntub.imd", "camping")
+        );
+        projectModule.addSerializer(UserRoleEnum.class, new JsonSerializer<>() {
+            @Override
+            public void serialize(UserRoleEnum value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+                gen.writeNumber(value.id);
+            }
+        });
+        projectModule.addDeserializer(UserRoleEnum.class, new JsonDeserializer<>() {
+            @Override
+            public UserRoleEnum deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+                return UserRoleEnum.getByRoleId(p.getValueAsInt());
+            }
+        });
+        projectModule.addSerializer(Gender.class, new JsonSerializer<>() {
+            @Override
+            public void serialize(Gender value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+                gen.writeString(value.id);
+            }
+        });
+        projectModule.addDeserializer(Gender.class, new JsonDeserializer<>() {
+            @Override
+            public Gender deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+                return Gender.getById(p.getValueAsString());
+            }
+        });
+        projectModule.addSerializer(Experience.class, new JsonSerializer<>() {
+            @Override
+            public void serialize(Experience value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+                gen.writeString(value.id);
+            }
+        });
+        projectModule.addDeserializer(Experience.class, new JsonDeserializer<>() {
+            @Override
+            public Experience deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+                return Experience.getById(p.getValueAsString());
+            }
+        });
+        mapper.registerModule(projectModule);
+        return mapper;
     }
 
     @Bean
