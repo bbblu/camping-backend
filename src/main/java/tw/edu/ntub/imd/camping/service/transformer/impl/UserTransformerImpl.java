250b69a9081dc117f98ffaf2404b83f9a0a570cd
EnWei <10646007@ntub.edu.tw>
2020-10-26 14:29:13 +0800

    修正 - 修正編譯錯誤的問題

--- src/main/java/tw/edu/ntub/imd/camping/service/transformer/impl/UserTransformerImpl.java
+++ src/main/java/tw/edu/ntub/imd/camping/service/transformer/impl/UserTransformerImpl.java
@@ -3,7 +3,6 @@ package tw.edu.ntub.imd.camping.service.transformer.impl;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import tw.edu.ntub.birc.common.util.JavaBeanUtils;
-import tw.edu.ntub.birc.common.util.StringUtils;
 import tw.edu.ntub.imd.camping.bean.UserBean;
 import tw.edu.ntub.imd.camping.databaseconfig.entity.User;
 import tw.edu.ntub.imd.camping.service.transformer.UserTransformer;
@@ -13,7 +12,6 @@ import javax.annotation.Nonnull;
 @AllArgsConstructor
 @Component
 public class UserTransformerImpl implements UserTransformer {
-    private final UserCommentDAO commentDAO;
 
     @Nonnull
     @Override
@@ -24,10 +22,6 @@ public class UserTransformerImpl implements UserTransformer {
     @Nonnull
     @Override
     public UserBean transferToBean(@Nonnull User user) {
-        UserBean result = JavaBeanUtils.copy(user, new UserBean());
-        if (StringUtils.isNotBlank(user.getAccount())) {
-            result.setComment((Double) commentDAO.getAverageCommentByUserAccount(user.getAccount()));
-        }
-        return result;
+        return JavaBeanUtils.copy(user, new UserBean());
     }
 }
