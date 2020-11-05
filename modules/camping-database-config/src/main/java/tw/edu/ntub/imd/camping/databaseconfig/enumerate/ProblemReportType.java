d7a23f017ccb239e6d1c0ba051d463f5d2cd67f0
EnWei <10646007@ntub.edu.tw>
2020-11-05 18:31:50 +0800

    更新問題回報的類型

--- modules/camping-database-config/src/main/java/tw/edu/ntub/imd/camping/databaseconfig/enumerate/ProblemReportType.java
+++ modules/camping-database-config/src/main/java/tw/edu/ntub/imd/camping/databaseconfig/enumerate/ProblemReportType.java
@@ -1,10 +1,33 @@
 package tw.edu.ntub.imd.camping.databaseconfig.enumerate;
 
 /**
- * 問題回報的問題類型
+ * 問題回報的問題類型(0: 帳號相關/ 1: 租借相關/ 2: 款項相關/ 3: 其他)
  *
  * @since 1.4.9
  */
 public enum ProblemReportType {
-    SYSTEM_ERROR
+    /**
+     * 帳號相關
+     *
+     * @since 1.4.9
+     */
+    ACCOUNT,
+    /**
+     * 租借相關
+     *
+     * @since 1.4.9
+     */
+    RENTAL,
+    /**
+     * 款項相關
+     *
+     * @since 1.4.9
+     */
+    BANK,
+    /**
+     * 其他
+     *
+     * @since 1.4.9
+     */
+    OTHER
 }
