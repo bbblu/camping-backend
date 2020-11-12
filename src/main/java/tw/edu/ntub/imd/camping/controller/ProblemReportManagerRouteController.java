1158d06560e35ab0042911ba94bdaf7385befea3
EnWei <10646007@ntub.edu.tw>
2020-11-12 10:19:32 +0800

    重命名 - ProblemReportRouteController -> ProblemReportManagerRouteController

--- src/main/java/tw/edu/ntub/imd/camping/controller/ProblemReportManagerRouteController.java
+++ src/main/java/tw/edu/ntub/imd/camping/controller/ProblemReportManagerRouteController.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(path = "/problem-report/manager")
-public class ProblemReportRouteController {
+public class ProblemReportManagerRouteController {
 
     @GetMapping(path = "")
     @PreAuthorize("hasAnyAuthority('Administrator', 'Manager')")
