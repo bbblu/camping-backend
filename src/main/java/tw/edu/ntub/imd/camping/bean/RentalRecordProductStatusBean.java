17b4a695967cb5ffefe65ea4b40a8ad4bdf916fb
EnWei <10646007@ntub.edu.tw>
2020-11-23 16:27:09 +0800

    修正 - 修正RentalRecordProductStatusBean缺少無參數建構元的問題

--- src/main/java/tw/edu/ntub/imd/camping/bean/RentalRecordProductStatusBean.java
+++ src/main/java/tw/edu/ntub/imd/camping/bean/RentalRecordProductStatusBean.java
@@ -3,10 +3,12 @@ package tw.edu.ntub.imd.camping.bean;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @Schema(name = "商品狀況")
 public class RentalRecordProductStatusBean {
